aa3d7bde7026ed73474133c1432eb8f2701fb631
Jameson <jamowelling@gmail.com>
2019-09-27 19:06:01 -0600

    create constants file

---
+++ src/constants.js
@@ -0,0 +1,27 @@
+export const conditions = [
+  {
+    value: 'scrap',
+    displayValue: 'Scrap',
+    multiplier: 0.80,
+  },
+  {
+    value: 'edgy',
+    displayValue: 'Edgy',
+    multiplier: 0.97,
+  },
+  {
+    value: 'average',
+    displayValue: 'Average',
+    multiplier: 1,
+  },
+  {
+    value: 'clean',
+    displayValue: 'Clean',
+    multiplier: 1.005,
+  },
+  {
+    value: 'spotless',
+    displayValue: 'Spotless',
+    multiplier: 1.06,
+  }
+];
